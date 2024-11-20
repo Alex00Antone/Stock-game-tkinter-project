679f91f6521af8e3381a71255c1f117202cb1a6a
Alex00Antone <alexjantone@gmail.com>
2024-11-20 15:04:33 -0700

    edited stock_class

--- stock_class.py
+++ stock_class.py
@@ -9,12 +9,12 @@ class Stock:
         self.history =self.generate_initial_history(20)
     
     def generate_initial_history(self, length):
-        # Generate initial prices based on the stock's initial price
-        price_range = 0.1 * self.price  # 10% of the initial price for variation
+        
+        price_range = 0.1 * self.price  
         return [round(random.uniform(self.price - price_range, self.price + price_range), 2) for _ in range(length)] + [self.price]
     
     def update_price(self):
-        change_percent = random.uniform(-0.05, 0.05)  # Price can change by ±5%
+        change_percent = random.uniform(-0.05, 0.05)  
         self.price = max(0, self.price * (1 + change_percent))
         self.history.append(self.price)
 
